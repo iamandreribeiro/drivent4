d38b12f84519473c871b74a45bbff600355e305d
iamandreribeiro <andreribeiroorafael@gmail.com>
2023-02-11 17:03:01 -0300

    feat get booking route

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -29,6 +29,24 @@ export async function postBooking(req: AuthenticatedRequest, res: Response) {
     }
 }
 
-export async function getBooking(req: AuthenticatedRequest, res: Response) {}
+export async function getBooking(req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+
+    try {
+        const booking = await bookingService.getBooking(userId);
+
+        res.status(200).send(booking);
+    } catch (error) {
+        if(error.name === "Forbidden") {
+            return res.sendStatus(httpStatus.FORBIDDEN);
+        }
+
+        if(error.name === "NotFoundError") {
+            return res.sendStatus(httpStatus.NOT_FOUND);
+        }
+        
+        return res.sendStatus(httpStatus.BAD_REQUEST);
+    }
+}
 
 export async function putBooking(req: AuthenticatedRequest, res: Response) {}
--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -3,7 +3,24 @@ import ticketRepository from "@/repositories/ticket-repository";
 import enrollmentRepository from "@/repositories/enrollment-repository";
 import bookingRepository from "@/repositories/booking-repository";
 
-async function getBooking() {}
+async function getBooking(userId: number) {
+    const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+    if(!enrollment) {
+        throw notFoundError();
+    }
+
+    const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+    if(!ticket || ticket.status === "RESERVED" || !ticket.TicketType.includesHotel || ticket.TicketType.isRemote) {
+        throw notFoundError();
+    }
+
+    const booking = await bookingRepository.findBookings(userId);
+    if(!booking) {
+        throw notFoundError();
+    }
+
+    return booking;
+}
 
 async function postBooking(userId:number, roomId: number) {
     const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
