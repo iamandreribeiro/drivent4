42b930a69b1928ea82124e1d572f33aeeb2efe77
iamandreribeiro <andreribeiroorafael@gmail.com>
2023-02-11 13:44:32 -0300

    feat booking service initial schema

---
+++ src/services/booking-service/index.ts
@@ -0,0 +1,13 @@
+async function getBooking() {}
+
+async function postBooking() {}
+
+async function putBooking() {}
+
+const bookingService = {
+    getBooking,
+    postBooking,
+    putBooking
+}
+
+export default bookingService;
